f79785c3d7e32d9d4638cd051ba47f5005d45460
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-09 09:42:04 -0300

    Criando Function de Load View Novo Usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function loadAllUsers()
     {
+        $all_users = User::all();
         return view('users');
+    }
+
+    public function loadAllUserForm()
+    {
+        return view('add-user');
     }
 }
