27529a2b695e6ef6cba83fbf756b2496500079fe
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-09 10:53:17 -0300

    Ajustando as Rotas

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get(
-    '/users',
-    [UserController::class, 'loadAllUsers']
-);
-Route::get(
-    '/add/user',
-    [UserController::class, 'loadAllUserForm']
-);
+Route::get('/users', [UserController::class, 'loadAllUsers']);
+Route::get( '/add/user', [UserController::class, 'loadAllUserForm']);
+
+Route::post('/add/user', [UserController::class, 'AddUser'])->name('AddUser');
