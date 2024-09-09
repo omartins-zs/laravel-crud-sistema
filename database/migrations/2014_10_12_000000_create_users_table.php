2c1b0561833899dbbbd560379b44e0679ed5b9d7
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-09 10:54:54 -0300

    Ajustando a Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome_completo');
             $table->string('email')->unique();
+            $table->string('celular')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('senha');
             $table->rememberToken();
             $table->timestamps();
         });
