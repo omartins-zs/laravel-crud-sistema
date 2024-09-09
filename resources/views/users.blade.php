a23ba6270b6c7dd87d8603876d8b5800e4a6d208
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-09 10:30:55 -0300

    Adicionando Alert de Sucesso

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -5,19 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravekl 11 Crud</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 
 <body>
-
-
     <div class="container">
         <div class="card">
             <div class="card-header">
                 Laravel 11 CRUD Sistema
                 <a href="/add/user" class="btn btn-success btn-sm float-end ">Cadastrar usuario</a>
             </div>
+            @if (Session::has('success'))
+                <span class="alert alert-success p-2">{{ Session::get('success') }}</span>
+            @endif
             <div class="card-body">
                 <table class="table table-sm table-striped table-bordered">
                     <thead>
@@ -36,7 +38,9 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 
 </body>
 
